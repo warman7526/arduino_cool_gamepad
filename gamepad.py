f819a866358c3d1a604d21257a7a78b158871930
warman7526 <warman7526@bradfordgrammar.com>
2024-05-30 17:23:56 +0100

    Update gamepad.py
    generally tidied up code with return types on some functions, and input validation for LCD.write()

--- gamepad.py
+++ gamepad.py
@@ -5,7 +5,6 @@
 
 
 import time
-from typing import Any
 import pyfirmata as pyfirmata
 
 # setup serial communication
@@ -15,7 +14,7 @@ board = pyfirmata.Arduino(PORT)
 
 time.sleep(4)  # give time for the arduino to connect and start before running code
 
-
+# not really sure why this is here but it breaks without it
 it = pyfirmata.util.Iterator(board)
 it.start()
 
@@ -25,12 +24,17 @@ class LCD:
 
     rarr = chr(0x7E)
     larr = chr(0x7F)
-    test = chr(0x91)
 
-    def __init__(self):
+    def __init__(self) -> None:
         self.line1, self.line2 = "", ""
 
-    def write(self, line1="", line2=""):
+    def write(self, line1="", line2="") -> None:
+        if type(line1) is not str or type(line2) is not str:
+            raise TypeError("line1 and line2 must be strings in LCD.write()")
+        if len(line1) > 16 or len(line2) > 16:
+            raise ValueError(
+                "line1 and line2 can be a maximum of 16 charachters in LCD.write()"
+            )
         self.line1 = line1
         self.line2 = line2
         board.send_sysex(
@@ -40,10 +44,10 @@ class LCD:
             pyfirmata.STRING_DATA, pyfirmata.util.str_to_two_byte_iter(line2)
         )
 
-    def clear(self):
+    def clear(self) -> None:
         self.write()
 
-    def get_text(self):
+    def get_text(self) -> list[str]:
         return [self.line1, self.line2]
 
 
@@ -69,7 +73,7 @@ class AnalogInput:
 
 
 class DigitalInput(AnalogInput):
-    def __init__(self, pin: int):
+    def __init__(self, pin: int) -> None:
         self.pin = board.get_pin(f"d:{pin}:i")
 
     def as_digital(self) -> bool:
@@ -84,10 +88,10 @@ class DigitalOutput:
         self.pin = board.get_pin(f"d:{pin}:o")
         self.pin.write(status)
 
-    def on(self):
+    def on(self) -> None:
         self.pin.write(1)
 
-    def off(self):
+    def off(self) -> None:
         self.pin.write(0)
 
 
@@ -96,10 +100,10 @@ class AnalogOutput:
         self.pin = board.get_pin(f"a:{pin}:o")
         self.pin.write(status)
 
-    def on(self, value=1023):
+    def on(self, value=1023) -> None:
         self.pin.write(value)
 
-    def off(self):
+    def off(self) -> None:
         self.pin.write(0)
 
 
@@ -128,36 +132,36 @@ class LEDs:
     blue = DigitalOutput(6)
     all = [cyan, white, red, yellow, green, blue]
 
-    def all_on():
-        return [LED.on() for LED in LEDs.all]
+    def all_on() -> None:
+        [LED.on() for LED in LEDs.all]
 
-    def all_off():
-        return [LED.off() for LED in LEDs.all]
+    def all_off() -> None:
+        [LED.off() for LED in LEDs.all]
 
 
 ##EXAMPLES
 
 # write text to the lcd (two lines of width 16 charachters)
 # only write to the LCD screen at least 150ms after previously writing as it may cause errors to occur that will not be caught by the code
-# lcd.write("text line 1","text line 2")
+lcd.write("text line 1", "text line 2")
 
 # clear LCD screen
-# lcd.clear()
+lcd.clear()
 
 # get text currently on the LCD screen ass array [line1,line2]
-# lcd.get_text()
+lcd.get_text()
 
 # turn on/off specific LED (colours are white, yellow, green, blue,red,cyan)
-# LEDs.blue.on()
-# LEDs.yellow.off()
+LEDs.blue.on()
+LEDs.yellow.off()
 
 # turn on/off ALL LEDs
-# LEDs.all_on()
-# LEDS.all_off()
+LEDs.all_on()
+LEDs.all_off()
 
 # get input from specific button (a,b,up,down,left,right => returns True when pressed, and False if not. returns None if there is an error)
-# button.a.as_digital()
-# button.left.as_digital()
+button.a.as_digital()
+button.left.as_digital()
 
 # get input from dpad as array of [up,down,left,right]
-# [b.as_digital() for b in button.dpad]
+[b.as_digital() for b in button.dpad]
