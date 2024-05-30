41a0452e7911f49a969f513704a5d8565301192e
warman7526 <warman7526@bradfordgrammar.com>
2024-05-30 17:13:26 +0100

    Update gamepad.py
    gamepad.py now works as a library to simplify the python needed to control the arduino

--- gamepad.py
+++ gamepad.py
@@ -1,17 +1,163 @@
-import pyfirmata2 as pyfirmata
+# Built using the firmata library for arduino
+# Built for python 3.10.11
+# tested on Arduino UNO-R3
+# install the modified version of firmata to the arduino before running this code ("StandardFirmata_lcdModification.ino")
 
-PORT = "COM6"
+
+import time
+from typing import Any
+import pyfirmata as pyfirmata
+
+# setup serial communication
+PORT = "COM6"  # change to whatever port is in use by your arduino
 board = pyfirmata.Arduino(PORT)
 
 
-write_lcd = lambda line1="", line2="": [
-    board.send_sysex(pyfirmata.STRING_DATA, pyfirmata.util.str_to_two_byte_iter(data))
-    for data in [line1, line2]
-]
+time.sleep(4)  # give time for the arduino to connect and start before running code
 
 
-class buttons:
-    up = board.get_pin("a:1:i")
-    down = board.get_pin("a:2:i")
-    left = board.get_pin("a:3:i")
-    right = board.get_pin("a:0:i")
+it = pyfirmata.util.Iterator(board)
+it.start()
+
+
+# interfaces to make it easier to run the arduinos
+class LCD:
+
+    rarr = chr(0x7E)
+    larr = chr(0x7F)
+    test = chr(0x91)
+
+    def __init__(self):
+        self.line1, self.line2 = "", ""
+
+    def write(self, line1="", line2=""):
+        self.line1 = line1
+        self.line2 = line2
+        board.send_sysex(
+            pyfirmata.STRING_DATA, pyfirmata.util.str_to_two_byte_iter(line1)
+        )
+        board.send_sysex(
+            pyfirmata.STRING_DATA, pyfirmata.util.str_to_two_byte_iter(line2)
+        )
+
+    def clear(self):
+        self.write()
+
+    def get_text(self):
+        return [self.line1, self.line2]
+
+
+class AnalogInput:
+    def __init__(self, pin: int) -> None:
+        self.pin = board.get_pin(f"a:{pin}:i")
+
+    def as_digital(self) -> bool:
+        return (
+            (self.pin.read() >= 0.7)
+            if self.pin.read() >= 0.7 or self.pin.read() <= 0.3
+            else None
+        )
+
+    def as_volts(self) -> float:
+        return self.pin.read() * 5
+
+    def as_analogInt(self) -> int:
+        return int(self.pin.read() * 1023)
+
+    def as_analog(self) -> float:
+        return self.pin.read()
+
+
+class DigitalInput(AnalogInput):
+    def __init__(self, pin: int):
+        self.pin = board.get_pin(f"d:{pin}:i")
+
+    def as_digital(self) -> bool:
+        return self.pin.read()
+
+    def as_analog(self) -> float:
+        return int(self.pin.read())
+
+
+class DigitalOutput:
+    def __init__(self, pin: int, status=0) -> None:
+        self.pin = board.get_pin(f"d:{pin}:o")
+        self.pin.write(status)
+
+    def on(self):
+        self.pin.write(1)
+
+    def off(self):
+        self.pin.write(0)
+
+
+class AnalogOutput:
+    def __init__(self, pin: int, status=0) -> None:
+        self.pin = board.get_pin(f"a:{pin}:o")
+        self.pin.write(status)
+
+    def on(self, value=1023):
+        self.pin.write(value)
+
+    def off(self):
+        self.pin.write(0)
+
+
+# sets up IO ports
+
+lcd = LCD()
+
+
+class button:
+    up = AnalogInput(1)
+    down = AnalogInput(2)
+    left = AnalogInput(3)
+    right = AnalogInput(0)
+    a = AnalogInput(5)
+    b = AnalogInput(4)
+    dpad = [up, down, left, right]
+    all = dpad + [a, b]
+
+
+class LEDs:
+    cyan = DigitalOutput(2)
+    white = DigitalOutput(3)
+    red = DigitalOutput(4)
+    yellow = DigitalOutput(5)
+    green = DigitalOutput(7)
+    blue = DigitalOutput(6)
+    all = [cyan, white, red, yellow, green, blue]
+
+    def all_on():
+        return [LED.on() for LED in LEDs.all]
+
+    def all_off():
+        return [LED.off() for LED in LEDs.all]
+
+
+##EXAMPLES
+
+# write text to the lcd (two lines of width 16 charachters)
+# only write to the LCD screen at least 150ms after previously writing as it may cause errors to occur that will not be caught by the code
+# lcd.write("text line 1","text line 2")
+
+# clear LCD screen
+# lcd.clear()
+
+# get text currently on the LCD screen ass array [line1,line2]
+# lcd.get_text()
+
+# turn on/off specific LED (colours are white, yellow, green, blue,red,cyan)
+# LEDs.blue.on()
+# LEDs.yellow.off()
+
+# turn on/off ALL LEDs
+# LEDs.all_on()
+# LEDS.all_off()
+
+# get input from specific button (a,b,up,down,left,right => returns True when pressed, and False if not. returns None if there is an error)
+# button.a.as_digital()
+# button.left.as_digital()
+
+# get input from dpad as array of [up,down,left,right]
+# [b.as_digital() for b in button.dpad]
